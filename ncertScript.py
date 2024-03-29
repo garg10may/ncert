e7dcf46a4374ac74defd1375d2dc6de1f08ebb68
garg10may <garg10may@gmail.com>
2024-03-29 11:09:49 +0530

    working writing combined pdf

--- ncertScript.py
+++ ncertScript.py
@@ -1,25 +1,3 @@
-#Write a script for site https://ncert.nic.in/textbook.php to download all the books of class 1 to 12.
-#The script should download all the books of all the subjects of all the classes.
-#The script should create a directory structure like:
-#ncert/
-#    class1/
-#        subject1/
-#            book1.pdf
-#            book2.pdf
-#        subject2/
-#            book1.pdf
-#            book2.pdf
-#    class2/
-#        subject1/
-#            book1.pdf
-#            book2.pdf
-#        subject2/
-#            book1.pdf
-#            book2.pdf
-#    .
-#    .
-#    .
-
 import requests
 from bs4 import BeautifulSoup
 import os
@@ -28,42 +6,70 @@ import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import shutil
+import zipfile
+from PIL import Image
+from PyPDF2 import PdfMerger
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def get_soup(url):
     response = requests.get(url)
     return BeautifulSoup(response.text, 'html.parser')
-  
-def get_classes(soup):
-    class_selector = soup.find('select', {'name': 'tclass'})
-    return class_selector.find_all('option')[1:]
-  
-def get_subjects(soup):
-    subjects = soup.find_all('div', class_='tab-content')
-    return subjects
-  
-def get_books(soup):
-    books = soup.find_all('div', class_='tab-content')
-    return books
-  
-def get_pdf_links(soup):
-    pdf_links = []
-    links = soup.find_all('a')
-    for link in links:
-        if link.get('href') and link.get('href').endswith('.pdf'):
-            pdf_links.append(link.get('href'))
-    return pdf_links
-  
+
+def create_directory(path):
+    os.makedirs('./' + path, exist_ok=True)
+    
+def extract_images_and_pdfs(zip_file, output_dir):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)
+
+def convert_image_to_pdf(image_path, output_pdf_path):
+    image = Image.open(image_path)
+    image.save(output_pdf_path, 'PDF', resolution=100.0, save_all=True)
+
+def combine_images_and_pdfs(dir, output_file):
+    cover_image = None
+    ps_pdf = None
+    chapter_pdfs = []
+
+    files = os.listdir(dir)
+
+    for file in files:
+        if file.endswith('.JPG'):
+            cover_image = file
+        elif file.endswith('ps.pdf'):
+            ps_pdf = file
+        elif file.endswith('.pdf') and not file.endswith('ps.pdf'):
+            chapter_pdfs.append(file)
+
+    if cover_image:
+        convert_image_to_pdf(os.path.join(dir, cover_image), os.path.join(dir, 'cover.pdf'))
+    # for names like gesc101.pdf, sort by the last two digits
+    print(chapter_pdfs)
+    chapter_pdfs.sort(key=lambda x: int(x.split('.')[0][-2:]))
+    merger = PdfMerger()
+
+    if cover_image:
+        all_pdfs = ['cover.pdf'] + [ps_pdf] + chapter_pdfs
+    else:
+        all_pdfs = [ps_pdf] + chapter_pdfs
+        
+    
+    for file in all_pdfs:
+        file_path = os.path.join(dir, file)
+        merger.append(file_path)
+
+    #write to parent directory
+    merger.write( os.path.join(dir, output_file))
+    # merger.write(output_file)
+    merger.close()
+
 def download_pdf(url, path):
     try:
         urllib.request.urlretrieve(url, path)
     except Exception as e:
         print(f'Error downloading {url}: {e}')
-
-
-def create_directory(path):
-    print(path)
-    os.makedirs('./' + path, exist_ok=True)
-    
         
 def main():
     url = 'http://ncert.nic.in/textbook.php'
@@ -71,9 +77,6 @@ def main():
     driver = webdriver.Chrome()
     driver.get(url)
 
-    #let the page load
-    # time.sleep(2)
-    
     class_select = driver.find_element(By.NAME, 'tclass')
     for class_option in class_select.find_elements(By.TAG_NAME, 'option')[1:2]:
       class_name = class_option.text
@@ -92,12 +95,23 @@ def main():
         for book_option in book_select.find_elements(By.TAG_NAME, 'option')[1:2]:
           book_name = book_option.text
           book_option.click()
-          print(book_name)
-          time.sleep(4)
           go_button = driver.find_element(By.XPATH, "//input[@name='button' and @value='Go']")
           go_button.click()
-          time.sleep(20)
-          pdf_link = driver.find_element(By.TAG_NAME, "a").get_attribute("href")
+
+          wait = WebDriverWait(driver, 10)
+          element = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(., 'Download complete book')]")))
+          book_link = element.get_attribute("href")
+          book_dir = os.path.join(subject_dir, book_name)
+          create_directory(book_dir)
+          download_pdf(book_link, os.path.join(book_dir, book_name + '.zip'))
+          driver.back()
+
+          zip_file = os.path.join(book_dir, book_name + '.zip')
+          extract_images_and_pdfs(zip_file, book_dir)
+          #delete zip file
+          os.remove(zip_file)
+          combine_images_and_pdfs(book_dir, os.path.join(book_name + '.pdf'))
+            
  
 if __name__ == '__main__':
   main()
